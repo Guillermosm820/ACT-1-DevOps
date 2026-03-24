afa3aef6eebdac4dfc1cc390e7ca481489bc3ae9
guillermo <al07038162@tecmilenio.mx>
2026-03-23 19:30:37 -0700

    Commit con entry

--- hola.py
+++ hola.py
@@ -1,7 +1,8 @@
-print("Hola Mundo")
-
 def nombre():
     nombre = "Guillermo Sagasta Mendoza"
     return nombre
 
-print(nombre())
+
+if __name__ == "__main__":
+    print("Hola Mundo")
+    print(nombre())
